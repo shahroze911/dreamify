f972ce1aaf3b1b21d41c999058df208bcd0208c1
Shahroze Kamran Sahotra <sksahotra911@gmail.com>
2024-08-10 16:00:53 +0500

    First Commit

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -1,4 +1,4 @@
-import { getClerkClient } from "@clerk/clerk-sdk-node"; // Correct import
+import { clerkClient } from "@clerk/clerk-sdk-node"; // Correct import
 import { WebhookEvent } from "@clerk/nextjs/server";
 import { headers } from "next/headers";
 import { NextResponse } from "next/server";
