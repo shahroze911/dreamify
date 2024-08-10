46f3bb156aef6de0cfa08555bc38e20a023f7805
Shahroze Kamran Sahotra <sksahotra911@gmail.com>
2024-08-10 12:46:13 +0500

    First Commit

---
+++ app/(root)/transformations/[id]/update/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const UpdateTransformationPage = () => {
+  return (
+    <div>UpdateTransformation
+        Page</div>
+  )
+}
+
+export default UpdateTransformationPage
